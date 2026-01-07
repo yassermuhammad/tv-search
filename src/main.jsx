67ee5ebf2905f9f7f9b6eda90a02fb8b0e42eaea
Yasser Abdel-Maksoud <yabdelmaksoud.c@tahakom.com>
2026-01-08 01:17:19 +0300

    Change default color mode to dark

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode="light" />
+    <ColorModeScript initialColorMode="dark" />
     <ChakraProvider>
       <App />
     </ChakraProvider>
